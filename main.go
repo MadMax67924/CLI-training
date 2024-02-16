3e7eed0c47c9e63489eabf2504f06400a7c11576
Maximiliano Garin <maxigarin06@gmail.com>
2024-02-16 09:29:37 -0300

    workin on gin-gonic integration

--- main.go
+++ main.go
@@ -1,51 +1,53 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 
-	"github.com/spf13/pflag"
-	//	"net/http"
-	// "github.com/gin-gonic/gin"
+	//"github.com/spf13/pflag"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-var (
-	mode   string
-	params []int
-)
+//var (
+//	mode   string
+//	params []int
+//)
 
 func main() {
-	//	r := gin.Default()
-	//	r.GET("/ping", func(c *gin.Context) {
-	//		c.JSON(http.StatusOK, gin.H{
-	//			"message": "pong",
-	//		})
-	//	})
-	//	r.Run()
-	pflag.StringVar(&mode, "mode", "", "Mode of the calculator to use")
-	pflag.IntSliceVar(&params, "params", []int{}, "Parameters")
-	pflag.Parse()
-	result := calculate()
-	fmt.Println(len(params))
-	fmt.Println("Results:", result)
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "chitito the best",
+		})
+	})
+	r.Run()
+	//pflag.StringVar(&mode, "mode", "", "Mode of the calculator to use")
+	//	pflag.IntSliceVar(&params, "params", []int{}, "Parameters")
+	//	pflag.Parse()
+	//	result := calculate()
+	//	fmt.Println(len(params))
+	//	fmt.Println("Results:", result)
 }
-func calculate() int {
-	result := params[0]
 
-	for i := 1; i < len(params); i++ {
-		switch mode {
-		case "sum":
-			result += params[i]
-		case "sustract":
-			result -= params[i]
-		case "multiply":
-			result *= params[i]
-		case "divide":
-			result /= params[i]
-		default:
-			fmt.Println("Operación no válida")
-			return 0
-		}
-	}
-
-	return result
-}
+//func calculate() int {
+//	result := params[0]
+//
+//	for i := 1; i < len(params); i++ {
+//		switch mode {
+//		case "sum":
+//			result += params[i]
+//		case "sustract":
+//			result -= params[i]
+//		case "multiply":
+//			result *= params[i]
+//		case "divide":
+//			result /= params[i]
+//		default:
+//			fmt.Println("Operación no válida")
+//			return 0
+//		}
+//	}
+//
+//	return result
+//}
