a01bb4e84e7c08411a39923c6762f77db1271900
Maximiliano Garin <maxigarin06@gmail.com>
2024-02-19 14:57:08 -0300

    added Get Post

--- main.go
+++ main.go
@@ -4,24 +4,43 @@ import (
 	//"fmt"
 
 	//"github.com/spf13/pflag"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-//var (
+// var (
+//
 //	mode   string
 //	params []int
-//)
+type data struct {
+	Mode   string `json:"mode"`
+	Params []int  `json:"params"`
+}
+
+var datas = []data{}
+
+func getdata(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, datas)
+}
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "ping",
-		})
+	r.GET("/calculator", getdata)
+	r.POST("/calculator", func(c *gin.Context) {
+		var ndata data
+		if err := c.BindJSON(&ndata); err != nil {
+			return
+		}
+		fmt.Println(datas)
+		datas = append(datas, ndata)
+		c.IndentedJSON(http.StatusCreated, ndata)
+		fmt.Println(datas)
+		fmt.Println("data created")
+		fmt.Println(ndata)
 	})
-	r.Run()
+	r.Run("localhost:8080")
 	//pflag.StringVar(&mode, "mode", "", "Mode of the calculator to use")
 	//	pflag.IntSliceVar(&params, "params", []int{}, "Parameters")
 	//	pflag.Parse()
@@ -31,6 +50,7 @@ func main() {
 }
 
 //func calculate() int {
+//
 //	result := params[0]
 //
 //	for i := 1; i < len(params); i++ {
